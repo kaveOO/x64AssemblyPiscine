0f5cedace8b395ebbcd74c3f4a8f3a62ee33ad88
kaveO <alban.billiette@gmail.com>
2025-07-18 22:46:54 +0200

    + | SubFunction to substract

---
+++ Arithmetic/Sub/Sub.asm
@@ -0,0 +1,11 @@
+global SubFunction
+
+section .text
+
+SubFunction:
+	mov rax, 10
+	mov rbx, 7
+	sub rax, rbx
+
+	ret
+
---
+++ Arithmetic/main.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int AddFunction();
+int SubFunction();
+
+int main()
+{
+	printf("%d\n", AddFunction());
+	printf("%d\n", SubFunction());
+}
