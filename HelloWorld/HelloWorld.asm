a1eaea7d83fe7b6311d1a86c2f192c9cde85b8d0
kaveO <alban.billiette@gmail.com>
2025-07-18 03:40:25 +0200

    + | Simple HelloWorld using hexa for sys_exit

---
+++ HelloWorld/HelloWorld.asm
@@ -0,0 +1,17 @@
+section .data
+    msg db "Hello, World", 0xA		-> 10 -> '\n'
+    len equ $ - msg
+
+section .text
+	global _start
+
+_start:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, len
+	syscall
+
+	mov rax, 0x3C	; 0011 1100 	-> 60 sys_exit
+	xor rdi, rdi
+	syscall
