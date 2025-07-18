e48520e9fc5c01e41ea181fb9bf9f3abb32866a6
kaveO <alban.billiette@gmail.com>
2025-07-19 00:55:28 +0200

    + | Added Neg function to get negative value of a number

---
+++ Arithmetic/Neg/Neg.asm
@@ -0,0 +1,9 @@
+global NegFunction
+
+section .text
+
+NegFunction:
+	mov rax, 42
+	neg rax
+
+	ret
--- Arithmetic/main.c
+++ Arithmetic/main.c
@@ -7,13 +7,15 @@ int IncFunction();
 int DecFunction();
 int MulFunction();
 int IMulFunction();
+int NegFunction();
 
 int main()
 {
-	printf("%d\n", AddFunction());
-	printf("%d\n", SubFunction());
-	printf("%d\n", IncFunction());
-	printf("%d\n", DecFunction());
-	printf("%d\n", MulFunction());
-	printf("%d\n", IMulFunction());
+	printf("Sums two numbers -> %d\n", AddFunction());
+	printf("Substract two numbers -> %d\n", SubFunction());
+	printf("Increment value in program %d\n", IncFunction());
+	printf("Decrement value in program -> %d\n", DecFunction());
+	printf("Multiplying unsigned integer -> %d\n", MulFunction());
+	printf("Multiplying signed integer -> %d\n", IMulFunction());
+	printf("Finding negative of a number -> %d\n", NegFunction());
 }
