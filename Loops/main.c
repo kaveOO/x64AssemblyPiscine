9018751613da38bae5b36f7c060edead8a0580dc
kaveOO <alban.billiette@gmail.com>
2025-07-19 18:31:50 +0200

    + | Added While loop function (calling it in C)

---
+++ Loops/main.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int While();
+// int For();
+// int DoWhile();
+
+int main()
+{
+	printf("%d\n", While());
+	// printf("%d\n", For());
+	// printf("%d\n", DoWhile());
+}
