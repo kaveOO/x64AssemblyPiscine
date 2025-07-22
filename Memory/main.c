a7929a7d8a9d1e70431f9b799748a0b9013cbd60
kaveOO <alban.billiette@gmail.com>
2025-07-22 17:41:45 +0200

    ~ | Using unsigned long long in main bcuz 64 bits x)

--- Memory/main.c
+++ Memory/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
 
-long long int 	Types();
-int 			Displacement();
-int				RetWords();
+long long int 		Types();
+int 				Displacement();
+unsigned long long	RetWords();
 
 int main()
 {
 	printf("%lld\n", Types());
 	printf("%d\n", Displacement());
-	printf("%d\n", RetWords());
+	printf("%llu\n", RetWords());
 }
